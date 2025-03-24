bcd45b183502b392c7b0791d86c49e857b206751
Cxrzin08 <felipepereirabritto08@gmail.com>
2025-03-24 14:33:03 -0300

    fix

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, jsonify, request
-
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=[])
 
-@app.route('/testfelipelindo', methods=['GET'])
-def Cliente():
-    return jsonify({"message:", "Teste kkk"})
+usuarios = []
+
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({'message': 'Hello, world!'})
 
 @app.route('/usuario/novo', methods=['POST'])
-def Novousuario():
+def criar_novo_usuario():
     novo_usuario = request.json
-    print(novo_usuario)
+    usuarios.append(novo_usuario)
     return jsonify({
         'user': novo_usuario,
-        'message': "Usuário criado com sucesso!"  
+        'message': 'Usuário criado com sucesso!'
     })
 
-
+@app.route('/usuarios', methods=['GET'])
+def listar_usuarios():
+    return jsonify({'usuarios': usuarios})
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(port=5000)
